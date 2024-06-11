506944500c2e6ad910dcc59e1389894666c18071
Tensofermi <93393604+Tensofermi@users.noreply.github.com>
2024-06-11 17:50:25 +0800

    test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 document.getElementById('mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
 });
