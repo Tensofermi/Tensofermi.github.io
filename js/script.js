5b24a9da7e36d7ab92d90982068029755f53ea92
Tensofermi <93393604+Tensofermi@users.noreply.github.com>
2024-08-02 14:52:40 +0800

    add pic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-document.getElementById('mode-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    document.querySelector('header').classList.toggle('dark-mode');
+const images_bg = [
+    './images/header-bg1.jpg',
+    './images/header-bg2.jpg',
+    './images/header-bg3.jpg' // 添加更多图片路径
+];
+
+let currentImageIndex = 1;
+
+const headerBg = document.querySelector('.header-bg');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+
+function updateBackgroundImage() {
+    headerBg.style.backgroundImage = `url('${images_bg[currentImageIndex]}')`;
+}
+
+prevBtn.addEventListener('click', () => {
+    currentImageIndex = (currentImageIndex === 0) ? images_bg.length - 1 : currentImageIndex - 1;
+    updateBackgroundImage();
 });
+
+nextBtn.addEventListener('click', () => {
+    currentImageIndex = (currentImageIndex === images_bg.length - 1) ? 0 : currentImageIndex + 1;
+    updateBackgroundImage();
+});
+
+updateBackgroundImage(); // 初始化背景图片
