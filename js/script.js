7fe6b036be3b08f648918e00ad6fd34cfe03ceae
Tensofermi <93393604+Tensofermi@users.noreply.github.com>
2024-08-04 08:54:02 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const images_bg = [
+    './images/header-bg0.jpg',
     './images/header-bg1.jpg',
     './images/header-bg2.jpg',
-    './images/header-bg3.jpg' // 添加更多图片路径
+    './images/header-bg3.jpg'
 ];
 
-let currentImageIndex = 2;
+let currentImageIndex = page_index;
 
 const headerBg = document.querySelector('.header-bg');
 const prevBtn = document.getElementById('prev-btn');
@@ -12,14 +13,20 @@ const nextBtn = document.getElementById('next-btn');
 
 function updateBackgroundImage() {
     headerBg.style.backgroundImage = `url('${images_bg[currentImageIndex]}')`;
+    // headerBg.classList.add('fade-in');
+    // setTimeout(() => headerBg.classList.remove('fade-in'), 500); // 动画持续500ms
 }
 
 prevBtn.addEventListener('click', () => {
+    const variable = "1233";
+    console.log(variable);
     currentImageIndex = (currentImageIndex === 0) ? images_bg.length - 1 : currentImageIndex - 1;
     updateBackgroundImage();
 });
 
 nextBtn.addEventListener('click', () => {
+    const variable = "1233";
+    console.log(variable);
     currentImageIndex = (currentImageIndex === images_bg.length - 1) ? 0 : currentImageIndex + 1;
     updateBackgroundImage();
 });
